1fd67ed97fd3672fdd4329600629495c6b16d577
dorog <dorogi.x@gmail.com>
2018-04-09 22:49:45 +0200

    New models
    New model: door, function is ready. Finish stone rename and new function (button for door)
    First real map.

---
+++ OnLab/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    private bool used;
+    private GameObject door;
+    private int count = 1;
+    public GameObject buttonModel;
+    public GameObject doorModel;
+    public GameObject brickModel;
+    public int mapNumber = 1;
+    GameObject parent;
+
+    // Use this for initialization
+    void Start()
+    {
+        used = false;
+        parent = GameObject.Find("MapGeneratorGO");
+        mapNumber = CurrentGameDatas.mapNumber;
+
+        switch (mapNumber)
+        {
+            case 1:
+                map1();
+                break;
+            default:
+                baseMap(10, 10);
+                break;
+        }
+    }
+
+    public void baseMap(int x, int z)
+    {
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < z; j++)
+            {
+                GameObject brick = Instantiate(brickModel, new Vector3(25 + i * 50, 0, 25 + j * 50), Quaternion.AngleAxis(-90, Vector3.right), parent.transform) as GameObject;
+            }
+        }
+        door = Instantiate(doorModel, new Vector3(25 + x * 50, 0, 325), Quaternion.AngleAxis(-90, Vector3.right), parent.transform) as GameObject;
+    }
+
+    public void map1()
+    {
+        count = 1;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                GameObject brick = Instantiate(brickModel, new Vector3(275 + i * 50, 0, 275 + j * 50), Quaternion.AngleAxis(-90, Vector3.right), parent.transform) as GameObject;
+            }
+            GameObject brick2 = Instantiate(brickModel, new Vector3(425 + i * 50, 0, 325), Quaternion.AngleAxis(-90, Vector3.right), parent.transform) as GameObject;
+        }
+
+        GameObject doorButton = Instantiate(buttonModel, new Vector3(475, 0, 375), Quaternion.AngleAxis(-90, Vector3.right), parent.transform) as GameObject;
+        door = Instantiate(doorModel, new Vector3(575, 0, 325), Quaternion.AngleAxis(-90, Vector3.right), parent.transform) as GameObject;
+
+    }
+
+    private void Update()
+    {
+        if (count == 0 && !used)
+        {
+            used = true;
+            Animation anim = door.GetComponent<Animation>();
+            anim.Play();
+        }
+    }
+
+    public void lessCount()
+    {
+        count--;      
+    }
+}
---
+++ OnLab/Assets/Scripts/SolvedButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolvedButton : MonoBehaviour {
+
+    private bool used;
+    MapGenerator mapGenerator;
+
+    // Use this for initialization
+    void Start()
+    {
+        mapGenerator = GameObject.Find("MapGeneratorGO").GetComponent<MapGenerator>();
+        used = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!used)
+        {
+            mapGenerator.lessCount();
+            used = true;
+        }
+    }
+}
